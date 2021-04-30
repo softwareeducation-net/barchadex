be249dc113872e750b78b834252d58dd114b420d
softwareeducation-net <47324217+softwareeducation-net@users.noreply.github.com>
2021-04-29 20:50:18 -0400

    new updates to balance page

--- src/pages/BalancesPage.tsx
+++ src/pages/BalancesPage.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Tabs } from 'antd';
 import {
-  useAllOpenOrdersBalances,
   useWalletBalancesForAllMarkets,
 } from '../utils/markets';
 import FloatingElement from '../components/layout/FloatingElement';
 import WalletBalancesTable from '../components/UserInfoTable/WalletBalancesTable';
 import { useMintToTickers } from '../utils/tokens';
-
+        import { useHistory, useLocation } from 'react-router-dom';
+        import { useWallet } from '../utils/wallet';
 const { TabPane } = Tabs;
 
 export default function BalancesPage() {
+        const { connected, wallet } = useWallet();
   const walletBalances = useWalletBalancesForAllMarkets();
   const mintToTickers = useMintToTickers();
-  const openOrdersBalances = useAllOpenOrdersBalances();
-
+        const location = useLocation();
+        const history = useHistory();
   const data = (walletBalances || []).map((balance) => {
     const balances = {
       coin: mintToTickers[balance.mint],
@@ -23,20 +24,24 @@ export default function BalancesPage() {
       openOrdersFree: 0,
       openOrdersTotal: 0,
     };
-    for (let openOrdersAccount of openOrdersBalances[balance.mint] || []) {
-      balances['openOrdersFree'] += openOrdersAccount.free;
-      balances['openOrdersTotal'] += openOrdersAccount.total;
-    }
+
     return balances;
   });
 
   return (
-    <FloatingElement style={{ flex: 1, paddingTop: 10 }}>
-      <Tabs defaultActiveKey="walletBalances">
-        <TabPane tab="Wallet Balances" key="walletBalances">
-          <WalletBalancesTable walletBalances={data} />
-        </TabPane>
-      </Tabs>
-    </FloatingElement>
+    <>
+        <FloatingElement style={{ flex: 1, paddingTop: 10 }}>
+
+        { connected && location.pathname == '/balances' && (
+
+                <Tabs defaultActiveKey="walletBalances">
+                  <TabPane tab="Wallet Balances" key="walletBalances">
+                    <WalletBalancesTable walletBalances={data} />
+                  </TabPane>
+                </Tabs>
+        )}
+        </FloatingElement>
+
+        </>
   );
 }
